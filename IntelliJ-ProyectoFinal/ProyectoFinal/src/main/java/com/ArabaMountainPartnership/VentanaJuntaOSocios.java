d7aa15b581972c639c4e5e00ffd3c1f67e747e24
Daniel Tamargo <danieltamargosaiz@gmail.com>
2019-05-20 15:29:06 +0200

    Proyecto IntelliJ

--- IntelliJ-ProyectoFinal/ProyectoFinal/src/main/java/com/ArabaMountainPartnership/VentanaJuntaOSocios.java
+++ IntelliJ-ProyectoFinal/ProyectoFinal/src/main/java/com/ArabaMountainPartnership/VentanaJuntaOSocios.java
@@ -1,4 +1,18 @@
 package com.ArabaMountainPartnership;
 
+import javax.swing.*;
+
 public class VentanaJuntaOSocios {
+    private JPanel panel;
+    private JButton verJuntaButton;
+    private JButton verSociosButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VentanaJuntaOSocios");
+        frame.setContentPane(new VentanaJuntaOSocios().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
